969b6c957d35ea0ce272c2f14b24dafb9d2ea48b
NMT456456 <NMT456456@gmail.com>
2025-12-06 19:17:44 +0700

    sync

--- dotfiles/nvim/lua/plugins/lualine.lua
+++ dotfiles/nvim/lua/plugins/lualine.lua
@@ -1,6 +1,7 @@
 return {
 	{
 		"nvim-lualine/lualine.nvim",
+
 		opts = function(_, opts)
 			opts.sections.lualine_z = {}
 			opts.options.theme = "OceanicNext"
--- dotfiles/nvim/lua/plugins/transparent.lua
+++ dotfiles/nvim/lua/plugins/transparent.lua
@@ -1,5 +1,6 @@
 return {
 	"xiyaowong/transparent.nvim",
+
 	config = function()
 		require("transparent").setup({
 			enable = true,
