f8b1f99700e6163abe28be9dc137b56cc01fce20
NMT456456 <NMT456456@gmail.com>
2025-12-07 16:24:02 +0700

    add markdown for nvim

--- dotfiles/nvim/lua/config/lazy.lua
+++ dotfiles/nvim/lua/config/lazy.lua
@@ -27,6 +27,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.json" },
 		{ import = "lazyvim.plugins.extras.lang.python" },
 		{ import = "lazyvim.plugins.extras.lang.tailwind" },
+		{ import = "lazyvim.plugins.extras.lang.markdown" },
 		{ import = "lazyvim.plugins.extras.util.mini-hipatterns" }, --plugin tailwind
 		{ import = "lazyvim.plugins.extras.ai.copilot" },
 		{ import = "lazyvim.plugins.extras.linting.eslint" },
